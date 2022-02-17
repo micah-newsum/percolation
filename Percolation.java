90b6d0e9a172521194c8f1336f47bd58b643423f
Micah Newsum <micah.newsum@glidewelldental.com>
2022-02-17 08:55:57 -0800

    First commit

---
+++ Percolation.java
@@ -0,0 +1,35 @@
+public class Percolation {
+    // creates n-by-b grid, with all sites initially blocked
+    public Percolation(int n){
+
+    }
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col){
+
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col){
+        return false;
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        return false;
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites(){
+        return -1;
+    }
+
+    // does the system percolate?
+    public boolean percolates(){
+        return false;
+    }
+
+    public static void main(String args[]){
+        System.out.println("Test client");
+    }
+}
