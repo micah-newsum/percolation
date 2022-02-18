6206f709249e64ddc6e0590843e258e37531887f
Micah Newsum <micah.newsum@glidewelldental.com>
2022-02-18 08:45:03 -0800

    Implement constructor and add argument checks to open, isOpen, and isFull methods

--- Percolation.java
+++ Percolation.java
@@ -1,21 +1,49 @@
 public class Percolation {
-    // creates n-by-b grid, with all sites initially blocked
-    public Percolation(int n){
+    boolean[][] sites;
 
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n){
+        if (n <= 0){
+            throw new IllegalArgumentException("n must be greater than 0");
+        }
+
+        sites = new boolean[n][n];
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                sites[i][j] = false;
+            }
+        }
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col){
-
+        if (row == 0 || row > sites.length) {
+            throw new IllegalArgumentException("Invalid row: must be between 1 & "+ sites.length);
+        } else if (col == 0 || col > sites.length) {
+            throw new IllegalArgumentException("Invalid column: must be between 1 & " + sites.length);
+        }
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col){
+        if (row == 0 || row > sites.length) {
+            throw new IllegalArgumentException("Invalid row: must be between 1 & "+ sites.length);
+        } else if (col == 0 || col > sites.length) {
+            throw new IllegalArgumentException("Invalid column: must be between 1 & " + sites.length);
+        }
+        
         return false;
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
+        if (row == 0 || row > sites.length) {
+            throw new IllegalArgumentException("Invalid row: must be between 1 & "+ sites.length);
+        } else if (col == 0 || col > sites.length) {
+            throw new IllegalArgumentException("Invalid column: must be between 1 & " + sites.length);
+        }
+        
+        
         return false;
     }
 
@@ -30,6 +58,7 @@ public class Percolation {
     }
 
     public static void main(String args[]){
-        System.out.println("Test client");
+        Percolation percolation = new Percolation(10);
+        percolation.open(10, 10);
     }
 }
