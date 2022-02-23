7d0cb6c6fab865569ea1b2b496d3e235732af0f4
Micah Newsum <micah.newsum@glidewelldental.com>
2022-02-23 09:02:54 -0800

    Add WeightQuickUnionUF data type

--- Percolation.java
+++ Percolation.java
@@ -1,5 +1,12 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
-    boolean[][] sites;
+    private WeightedQuickUnionUF uf;
+    private boolean sites[][];
+    private int virtualTopSite;
+    private int virtualBottomSite;
+    private int numberOfOpenSites;
+    private int n;
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n){
@@ -7,58 +14,54 @@ public class Percolation {
             throw new IllegalArgumentException("n must be greater than 0");
         }
 
+        this.n = n;
+        int totalSites = n^2 + 2;
+        virtualTopSite = totalSites - 2;
+        virtualBottomSite = totalSites - 1;
+        uf = new WeightedQuickUnionUF(totalSites);
         sites = new boolean[n][n];
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++){
-                sites[i][j] = false;
-            }
-        }
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col){
-        if (row == 0 || row > sites.length) {
-            throw new IllegalArgumentException("Invalid row: must be between 1 & "+ sites.length);
-        } else if (col == 0 || col > sites.length) {
-            throw new IllegalArgumentException("Invalid column: must be between 1 & " + sites.length);
-        }
+        check(row, col);
+        sites[row - 1][col - 1] = true;
+        // check if adjacent sites are also open, and if so, connect.
+        numberOfOpenSites++;
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col){
-        if (row == 0 || row > sites.length) {
-            throw new IllegalArgumentException("Invalid row: must be between 1 & "+ sites.length);
-        } else if (col == 0 || col > sites.length) {
-            throw new IllegalArgumentException("Invalid column: must be between 1 & " + sites.length);
-        }
-        
-        return false;
+        check(row, col);
+        return sites[row - 1][col - 1];
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        if (row == 0 || row > sites.length) {
-            throw new IllegalArgumentException("Invalid row: must be between 1 & "+ sites.length);
-        } else if (col == 0 || col > sites.length) {
-            throw new IllegalArgumentException("Invalid column: must be between 1 & " + sites.length);
-        }
-        
-        
-        return false;
+        check(row, col);
+        return sites[row - 1][col - 1];
+    }
+
+    private void check(int row, int col){
+        if (row == 0 || row > n) {
+            throw new IllegalArgumentException("Invalid row: must be between 1 & "+ n);
+        } else if (col == 0 || col > n) {
+            throw new IllegalArgumentException("Invalid column: must be between 1 & " + n);
+        } 
     }
 
     // returns the number of open sites
     public int numberOfOpenSites(){
-        return -1;
+        return numberOfOpenSites;
     }
 
     // does the system percolate?
     public boolean percolates(){
-        return false;
+        return uf.connected(virtualTopSite, virtualBottomSite);
     }
 
     public static void main(String args[]){
-        Percolation percolation = new Percolation(10);
+        Percolation percolation = new Percolation(3);
         percolation.open(10, 10);
     }
 }
