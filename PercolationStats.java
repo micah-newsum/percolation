d45c0be5a4e0957c1caf1b2587a323a6dbf19f50
Micah Newsum <micah.newsum@glidewelldental.com>
2022-03-02 09:00:42 -0800

    Throw illegal argument exception if n and T are less than or equal to 0

--- PercolationStats.java
+++ PercolationStats.java
@@ -1,7 +1,12 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 public class PercolationStats {
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
-
+        if (n <= 0 || trials <= 0){
+            throw new IllegalArgumentException();
+        }
     }
 
     // sample mean of percolation threshold
@@ -26,6 +31,21 @@ public class PercolationStats {
 
     // test client
     public static void main(String[] args){
+        int n = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
+        double[] simulations = new double[T];
 
+        double totalSites = Math.pow(n, 2);
+
+        for (int i = 0; i < T; i++){
+            Percolation percolation = new Percolation(n);
+            while (!percolation.percolates()){
+                int row = StdRandom.uniform(1, n + 1);
+                int col = StdRandom.uniform(1, n + 1);
+                percolation.open(row, col);
+            }
+            simulations[i] = ((double) percolation.numberOfOpenSites() / totalSites);
+        }
+        System.out.println(simulations);
     }
 }
