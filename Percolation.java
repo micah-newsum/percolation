5e62a4970bd2064769ebb136b05fc791195db6bb
Micah Newsum <micah.newsum@glidewelldental.com>
2022-03-08 09:11:12 -0800

    Fix bug in getSite

--- Percolation.java
+++ Percolation.java
@@ -24,19 +24,24 @@ public class Percolation {
         // connect all of top row to virtualTopSite
         for (int i = 1; i <= n; i++) {
             int p = getSite(1, i);
+            // System.out.println(p);
+            // System.out.println(virtualTopSite);
             uf.union(p, virtualTopSite);
         }
 
         // connect all of bottom row to virtualBottomSite
         for (int i = 1; i <= n; i++) {
             int p = getSite(n, i);
+            System.out.printf("row=%d, col=%d\n",n, i);
+            System.out.println(p);
+            // System.out.println(virtualBottomSite);
             uf.union(p, virtualBottomSite);
         }
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
-        check(row, col);
+        check(row, col); 
         
         if (isOpen(row, col)){
             return;
@@ -76,8 +81,8 @@ public class Percolation {
         numberOfOpenSites++;
     }
 
-    private int getSite(int row, int col){
-        return (row - 1) * 3 + (col - 1);
+    public int getSite(int row, int col){
+        return (row - 1) * n + (col - 1);
     }
 
     private boolean isValidSite(int row, int col){
@@ -114,8 +119,18 @@ public class Percolation {
         return uf.connected(virtualTopSite, virtualBottomSite);
     }
 
+    public void print(){
+        for(int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                String site = sites[i][j] ? "X" : "O";
+                System.out.print(" "+site+" ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String args[]){
-        Percolation percolation = new Percolation(3);
+        // Percolation percolation = new Percolation(3);
 
         // Change access to public to test
         // System.out.println(percolation.getSite(1, 1)); // expect 0
@@ -123,34 +138,47 @@ public class Percolation {
         // System.out.println(percolation.getSite(3, 3)); // expect 8
 
         // test opening outside boundaries
-        try {
-            percolation.isOpen(0, 0);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e);
-        }
+        // try {
+        //     percolation.isOpen(0, 0);
+        // } catch (IllegalArgumentException e) {
+        //     System.out.println(e);
+        // }
 
-        try {
-            percolation.isOpen(4, 4);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e);
-        }
+        // try {
+        //     percolation.isOpen(4, 4);
+        // } catch (IllegalArgumentException e) {
+        //     System.out.println(e);
+        // }
 
-        percolation.open(1, 1);
-        percolation.open(2, 1);
-        percolation.open(3, 1);
-        System.out.println("System percolates: "+percolation.percolates()); // expect true
+        // percolation.open(1, 1);
+        // percolation.open(2, 1);
+        // percolation.open(3, 1);
+        // System.out.println("System percolates: "+percolation.percolates()); // expect true
+        // percolation.print();
 
-        // test full site
-        System.out.println("Site is full: "+percolation.isFull(2, 2)); // expect true
-        System.out.println("Site is open: "+percolation.isOpen(2, 1)); // expect true
+        // // test full site
+        // System.out.println("Site is full: "+percolation.isFull(2, 2)); // expect true
+        // System.out.println("Site is open: "+percolation.isOpen(2, 1)); // expect true
 
-        // open remaining sites and check if full
-        percolation.open(2, 1);
-        percolation.open(2, 2);
-        percolation.open(2, 3);
-        percolation.open(3, 1);
-        percolation.open(3, 2);
-        percolation.open(3, 3);
-        System.out.printf("%d out of 9 sites open\n",percolation.numberOfOpenSites());
+        // // open remaining sites and check if full
+        // percolation.open(2, 1);
+        // percolation.open(2, 2);
+        // percolation.open(2, 3);
+        // percolation.open(3, 1);
+        // percolation.open(3, 2);
+        // percolation.open(3, 3);
+        // System.out.printf("%d out of 9 sites open\n",percolation.numberOfOpenSites());
+        
+        int n = 4;
+        Percolation percolation = new Percolation(n);
+        System.out.println(percolation.getSite(4, 1)); // expect 12
+        System.out.println(percolation.getSite(4, 2)); // expect 13
+        System.out.println(percolation.getSite(4, 3)); // expect 14
+        System.out.println(percolation.getSite(4, 4)); // expect 15
+        // for (int i = 1; i < n; i++){
+        //     percolation.open(i, 4);
+        // }
+        // System.out.println("Percolates: "+percolation.percolates());
+        // percolation.print();
     }
 }
