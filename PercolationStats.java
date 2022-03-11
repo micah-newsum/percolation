84566520f4c60c3a50640563413b0b4f0ed9f98c
Micah Newsum <micah.newsum@glidewelldental.com>
2022-03-11 08:46:31 -0800

    Implement methods

--- PercolationStats.java
+++ PercolationStats.java
@@ -2,50 +2,58 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
+    double simulations[];
+    int trials;
+
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
         if (n <= 0 || trials <= 0){
             throw new IllegalArgumentException();
         }
+        
+        simulations = new double[trials];
+        this.trials = trials; 
+
+        double totalSites = Math.pow(n, 2);
+
+        for (int i = 0; i < trials; i++){
+            Percolation percolation = new Percolation(n);
+            
+            while (!percolation.percolates()){
+                int row = StdRandom.uniform(n) + 1;
+                int col = StdRandom.uniform(n) + 1;
+                percolation.open(row, col);
+            }
+            simulations[i] = (double) percolation.numberOfOpenSites() / totalSites;
+        }
     }
 
     // sample mean of percolation threshold
     public double mean(){
-        return 0.0;
+        return StdStats.mean(simulations);
     }
 
     // sample standard deviation of percolation threshold
     public double stddev(){
-        return 0.0;
+        return StdStats.stddev(simulations);
     }
 
     // low endpoint of 95% confidence interval
     public double confidenceLo(){
-        return 0.0;
+        return mean() - ((1.96 * stddev()) / Math.sqrt(trials));
     }
 
     // high endpoint of 95% confidence interval
     public double confidenceHi(){
-        return 0.0;
+        return mean() + ((1.96 * stddev()) / Math.sqrt(trials));
     }
 
     // test client
     public static void main(String[] args){
-        int n = Integer.parseInt(args[0]);
-        int T = Integer.parseInt(args[1]);
-        double[] simulations = new double[T];
-
-        double totalSites = Math.pow(n, 2);
-
-        for (int i = 0; i < T; i++){
-            Percolation percolation = new Percolation(n);
-            while (!percolation.percolates()){
-                int row = StdRandom.uniform(1, n + 1);
-                int col = StdRandom.uniform(1, n + 1);
-                percolation.open(row, col);
-            }
-            simulations[i] = ((double) percolation.numberOfOpenSites() / totalSites);
-        }
-        System.out.println(simulations);
+        PercolationStats stats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        System.out.println(stats.mean());
+        System.out.println(stats.stddev());
+        System.out.println(stats.confidenceLo());
+        System.out.println(stats.confidenceHi());
     }
 }
