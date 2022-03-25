3f32cea2534485416fc2436a9d867d49a4f9449c
Micah Newsum <micah.newsum@glidewelldental.com>
2022-03-25 09:01:21 -0700

    Set static variable at top of class.

--- PercolationStats.java
+++ PercolationStats.java
@@ -2,9 +2,9 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
+    private final static double CONFIDENCE_95 = 1.96;
     private final double[] simulations;
     private final int trials;
-    private final static double CONFIDENCE_95 = 1.96;
 
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
